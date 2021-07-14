3afc02ff464211be0510e9dba4393d7d6e38d892
marianagudr <marianagudr@gmail.com>
2021-07-14 09:49:17 -0300

    Modificacion .js

--- main.js
+++ main.js
@@ -1,27 +1,49 @@
-class Pedido {
-    constructor(tipo, tamano) {
+class Producto {
+    constructor(tipo, tamano, precio) {
         this.tipo = tipo;
         this.tamano = tamano;
+        this.precio = precio;
     }
 
     toString() {
-        console.log(`Tipo de Ilustración: ${this.tipo}\nTamaño (cm): ${this.tamano}`);
+        console.log(`Tipo de Ilustración: ${this.tipo}\nTamaño (cm): ${this.tamano}\nPrecio: $${this.precio}`);
     }
+}
 
-    eleccion() {
-        switch (tipo) {
+class Tienda {
+    constructor(nombre){
+        this.nombre = nombre;
+        this.productos = [];
+    }
+    agregarProductos(producto){
+        this.productos.push(producto);
+    }
+}
+
+let precio = 0;
+let tiendaOnLine = new Tienda("Tienda online");
+let opcion = prompt("Ingresá 1, si deseas iniciar/continuar tu pedido o, Enter para salir");
+
+while (opcion == "1") {
+    const tipo = prompt("Ingresá el tipo de ilustración que deseas (digital o artesanal):");
+    const tamano = prompt("¿Qué tamaño deseás (A4, 20x30 ó 30x40?):");
+
+    switch (tipo) {
             case "artesanal":
-                console.log("El precio de la ilustración artesanal es $6000 y solo viene en tamaño A4");
+                if (tamano == "A4" || tamano == "a4") {
+                    precio = 6000;
+                }
+                else {
+                    precio = 0;
+                }
                 break;
             
             case "digital":
-                if ((tamano == "A4" || tamano == "a4") || tamano == "20x30"){
-                    console.log("El precio de la ilustración digital es $4000");
-                    console.log("Por favor rellená el formulario y al confirmar el pedido recibirás un correo electrónico. Gracias!!");
+                if ((tamano == "A4" || tamano == "a4") || tamano == "20x30") {
+                    precio = 4000;
                 }
-                else if (tamano == "30x40"){
-                    console.log("El precio de la ilustración digital es $5000");
-                    console.log("Por favor rellená el formulario y al confirmar el pedido recibirás un correo electrónico. Gracias!!");
+                else if (tamano == "30x40") {
+                    precio = 5000;
                 }
                 else {
                     console.log("Refresque el navegador y vuelva a ingresar los datos");
@@ -31,12 +53,19 @@ class Pedido {
             default:
                 console.log("Refresque el navegador y vuelva a ingresar los datos");
         }
-    }
+
+    let pedido1 = new Producto(tipo, tamano, precio);
+    
+    pedido1.toString();
+    tiendaOnLine.agregarProductos(pedido1);
+
+    opcion = prompt("Ingresá 1, si deseas iniciar/continuar tu pedido o, Enter para salir");
 }
 
-const tipo = prompt("Ingresá el tipo de ilustración que deseas (digital o artesanal):");
-const tamano = prompt("¿Qué tamaño deseás (A4, 20x30 ó 30x40?):");
-const pedido1 = new Pedido(tipo, tamano);
+console.log(tiendaOnLine);
 
-pedido1.toString();
-pedido1.eleccion();
+let totalPedido = tiendaOnLine.reduce((acumulador, tiendaOnLine[precio]) => acumulador + tiendaOnLine[precio], 0);
+    
+console.log("El total de tu compra es " + totalPedido);
+
+
